b516398f687e30b7375426a7554a9731aa94d7d5
Thomas Andolf <thomas.andolf@gmail.com>
2019-06-19 02:53:29 +0200

    Initial Commit

---
+++ GameOfLife/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GameOfLife
+//
+//  Created by Thomas Andolf on 2019-06-19.
+//  Copyright © 2019 Thomas Andolf. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
