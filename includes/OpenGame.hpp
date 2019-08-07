46a060c7bfb53a162985b72b8be54942c203c5db
Thomas Andolf <thomas.andolf@gmail.com>
2019-08-07 16:06:57 +0200

    changed so that size is set by tiles and not by pixels

---
+++ includes/OpenGame.hpp
@@ -0,0 +1,13 @@
+//
+//  OpenGame.hpp
+//  GameOfLife
+//
+//  Created by Thomas Andolf on 2019-08-05.
+//  Copyright © 2019 Thomas Andolf. All rights reserved.
+//
+
+#ifndef OpenGame_h
+#define OpenGame_h
+
+
+#endif /* OpenGame_h */
